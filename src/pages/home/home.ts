2bd6002285baa9ef035f969d0cf68a91018df183
Julio Briceno <juliob@ptree.com.mx>
2017-08-31 07:30:23 -0500

    Adaptaciones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuopcionsPage } from "../index.paginas";
+import { Http } from '@angular/http';
+import { AlertController, LoadingController } from 'ionic-angular';
+
+import { url } from "../../config/url.config"
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,8 +13,37 @@ import { MenuopcionsPage } from "../index.paginas";
 export class HomePage {
   tab10:any = MenuopcionsPage;
   menu:string="alert";
-  constructor(public navCtrl: NavController) {
+  devices:any = [];
+  constructor(public navCtrl: NavController,
+             public http: Http, public alertCtrl: AlertController,
+             public loadingCtrl: LoadingController) {
+    let mUrl = url + 'api/GetInitialData';
+    const body = {userLogon: { strEmail: '', strPassword: '' }};
 
+    let loading = this.loadingCtrl.create({
+      content: 'Working...',
+      spinner: 'ios'
+    });
+
+    loading.present();
+    this.http
+      .post( mUrl, body ).subscribe(res => {
+        loading.dismiss();
+        console.log(res.json().Result);
+        if (res.json().Result == 'ok' ){
+          this.devices = res.json().Devices;
+          alert('Vale que bien');
+          console.log(this.devices);
+        }
+        else{
+          let alert = this.alertCtrl.create({
+            title: 'Oops!',
+            subTitle: 'System error',
+            buttons: ['Ok']
+          });
+          alert.present();        }
+      }
+    )
   }
 
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TabsPage } from "../index.paginas";
+import { TabsPage, HomePage } from "../index.paginas";
+import { AlertController, LoadingController } from 'ionic-angular';
 
 import { url } from "../../config/url.config"
 
@@ -14,20 +15,40 @@ export class LoginPage {
   tab8:any = TabsPage;
   segments:string="login";
   constructor(public navCtrl: NavController, public navParams: NavParams,
-             public http: Http) {
+             public http: Http, public alertCtrl: AlertController,
+             public loadingCtrl: LoadingController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+  correo:string = '';
+  contrasena:string = '';
 
   Login(){
     let mUrl = url + 'Logon';
 
-    const body = {userLogon: { strEmail: 'julio.briceno@gmail.com', strPassword: '333' }};
+    const body = {userLogon: { strEmail: this.correo, strPassword: this.contrasena }};
+
+    let loading = this.loadingCtrl.create({
+      content: 'Working...',
+      spinner: 'ios'
+    });
+
+    loading.present();
 
     this.http
-      .post( mUrl, body ).subscribe(res => alert(res.json().Result ));
+      .post( mUrl, body ).subscribe(res => {
+        loading.dismiss();
+        if (res.json().Result == 'ok' ){
+          this.navCtrl.push( HomePage )
+        }
+        else{
+          let alert = this.alertCtrl.create({
+            title: 'Oops!',
+            subTitle: 'Credentials are invalid! Please register',
+            buttons: ['Ok']
+          });
+          alert.present();        }
+      }
+    )
   }
 
 }
